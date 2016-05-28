a7e80b581571943959acd1b8695e5c1414b0abb4
donghyo <eotkdehwnd12@naver.com>
2016-05-28 23:51:46 +0900

    no message

---
+++ IDhackathon/app/src/main/java/idhackathon/com/idhackathon/MemoDailyActivity.java
@@ -0,0 +1,16 @@
+package idhackathon.com.idhackathon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jungminki on 2016-05-28.
+ */
+public class MemoDailyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memodaily);
+    }
+
+}
