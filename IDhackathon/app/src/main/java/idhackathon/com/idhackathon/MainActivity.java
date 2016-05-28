b791524b9eb284342137f0e600955268501d9b34
yh-kim <btc_yh@naver.com>
2016-05-29 03:00:33 +0900

    ㅇㄴㄹ

--- IDhackathon/app/src/main/java/idhackathon/com/idhackathon/MainActivity.java
+++ IDhackathon/app/src/main/java/idhackathon/com/idhackathon/MainActivity.java
@@ -2,6 +2,8 @@ package idhackathon.com.idhackathon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,6 +16,9 @@ import idhackathon.com.idhackathon.adapter.ScheduleListAdapter;
 import idhackathon.com.idhackathon.items.ScheduleItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigationView;
+
     ListView lvMainSchedule;
     ScheduleListAdapter adapter;
     ArrayList<ScheduleItem> arrSchedule = new ArrayList<>();
